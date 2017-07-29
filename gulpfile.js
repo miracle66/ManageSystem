f02f0bfda065d721109530c0b441ebacf0ef97e6
lily <13735812566@163.com>
2017-07-29 17:43:22 +0800

    gulp 相关

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create()
 
 gulp.task('sass',function(){
     return gulp.src('src/web.scss')
-        .pipe( sass() ).pipe( gulp.dest( 'css' ) );
-});
+        .pipe(sass())
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.stream({match: '**/*.css'}))
+})
 
 // gulp.task('js', function() {
 //     return gulp.src('js/ihmaMain.js')
@@ -13,13 +16,25 @@ gulp.task('sass',function(){
 //         .pipe(gulp.dest('dist/js'))    //输出main.js到文件夹
 //         .pipe(rename({suffix: '.min'}))   //rename压缩后的文件名
 //         .pipe(uglify())    //压缩
-//         .pipe(gulp.dest('dist/js'));  //输出
-// });
+//         .pipe(gulp.dest('dist/js'))  //输出
+// })
+
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: {
+          baseDir: ".",
+          directory: true,
+        }
+    })
+
+    gulp.watch("src/*.scss", ['sass'])
+    gulp.watch("src/*.html").on('change', browserSync.reload)
+})
 
 //默认任务
-gulp.task('default',['sass']);
+gulp.task('default',['sass'])
 
 //监听文件
 gulp.task('watch',function () {
-    return gulp.watch('src/web.scss',['sass']);
-});
+    return gulp.watch('src/web.scss',['sass'])
+})
